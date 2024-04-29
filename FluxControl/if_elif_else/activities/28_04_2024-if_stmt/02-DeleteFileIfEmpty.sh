9ee315d60e06f5ad7051e1cf862dc39140de76a4
AsherLucRen <82063404+LucasDiabloChan@users.noreply.github.com>
2024-04-29 16:35:45 -0300

    Update 02-DeleteFileIfEmpty.sh

--- FluxControl/if_elif_else/activities/28_04_2024-if_stmt/02-DeleteFileIfEmpty.sh
+++ FluxControl/if_elif_else/activities/28_04_2024-if_stmt/02-DeleteFileIfEmpty.sh
@@ -5,10 +5,22 @@
 # Description (en-us): receive a file name and, if it's empty, delete it. But ask to the user, before that.
 # Descrição (pt-br): criar um script que recebe o nome de um arquivo e se o mesmo não tiver conteúdo apaga, mas pede permissão para o usuario.
 
-echo "Type a filename to delete: "
-read fileName
-
-if [ -e $fileName]
-then
-    echo "file does not exists!"
+if [ ! -e $1 ]; then
+    echo "The file $1 does not exists."
 fi
+
+if [ -f $1 -a ! -s $1 ]; then
+    echo "Can we delete the file '$1'? (Y/n)"
+    read userChoice
+
+    if [ $userChoice="Y" -o $userChoice="y" ]; then
+        rm $1
+        echo "File deleted successfully!"
+    else
+        echo "The file was not deleted."
+    fi
+
+    exit 6
+fi
+
+echo "The file must be empty to delete it!"
