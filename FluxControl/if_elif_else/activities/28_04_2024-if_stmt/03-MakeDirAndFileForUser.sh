6c6a68b79848dc1967117213aba130e51ad74219
AsherLucRen <82063404+LucasDiabloChan@users.noreply.github.com>
2024-04-29 17:48:04 -0300

    Update 03-MakeDirAndFileForUser.sh

--- FluxControl/if_elif_else/activities/28_04_2024-if_stmt/03-MakeDirAndFileForUser.sh
+++ FluxControl/if_elif_else/activities/28_04_2024-if_stmt/03-MakeDirAndFileForUser.sh
@@ -14,7 +14,7 @@
 #    |--> um diretório e um arquivo, que devem ser defini-
 #    |--> dos pelo usuário, através de menu com 4 opções
 #    |--> de nomes. Use a estrutura 'case'
-# Calling method: ./<exeArchiveName> <dir> <file>
+# Calling method: ./<exeArchiveName> <dir>
 
 actualDir=$(pwd)
 
@@ -23,18 +23,46 @@ if [ -e $1 ]; then
         exit 0
 fi
 
-if [ -e $actualDir/$1/$2 ]; then
+if [ -e $actualDir/$1 ]; then
         echo "The file '$2 already exists!'"
         exit 0
 fi
 
-if [ -z $2 ]; then
-        echo "Invalid archive name (second parameter)."
-        exit 0
-fi
-
+echo "# - - - - - - - - - - - - - - - - - - - - #"
 mkdir $1
 echo "Directory '$1' created"
+echo ""
 
-touch $actualDir/$1/$2
-echo "File created in '~/$1/$2'"ahser_ren:
+
+name1="MyFrickingDumbLife.txt"
+name2="EverebodyAreBirds.xml"
+name3="WhoPeedInMyJacket.ipynb"
+name4="BroGotOhioRizz.exe"
+
+echo "choose a name for the archive (1-4): "
+echo "(1) - $name1"
+echo "(2) - $name2"
+echo "(3) - $name3"
+echo "(4) - $name4"
+read userChoice
+echo ""
+
+case $userChoice in
+        "1")
+            touch $actualDir/$1/$name1
+            echo "File created in '~/$1/$name1'"
+            ;;
+        "2")
+            touch $actualDir/$1/$name2
+            echo "File created in '~/$1/$name2'"
+            ;;
+        "3")
+            touch $actualDir/$1/$name3
+            echo "File created in '~/$1/$name3'"
+            ;;
+        "4")
+            touch $actualDir/$1/$name4
+            echo "File created in '~/$1/$name4'"
+            ;;
+esac
+echo "# - - - - - - - - - - - - - - - - - - - - #"
